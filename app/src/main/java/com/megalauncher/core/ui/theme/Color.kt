66f55fac31ef2109f6c5b9b9b6f635deedcae97e
aliminnikita-cmd <alimin.nikita@gmail.com>
2026-09-23 16:54:09 +0500

    Create Color.kt

---
+++ app/src/main/java/com/megalauncher/core/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.megalauncher.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Accent = Color(0xFF6C5CE7)
+val AccentDim = Color(0xFF4834D4)
+val BgDark = Color(0xFF0E0E12)
+val SurfaceDark = Color(0xFF1A1A22)
+val SurfaceDark2 = Color(0xFF23232E)
+val TextPrimary = Color(0xFFF5F5F7)
+val TextSecondary = Color(0xFFA0A0B0)
+
+val CarBg = Color(0xFF000000)
+val CarSurface = Color(0xFF141414)
+val CarAccent = Color(0xFFFFB300)
